e67f7030e13678166605ca71c5124d7f35fc40b6
Sergey-T-ban <s.trunka@e-u.edu.ua>
2025-11-09 19:37:39 +0200

    P2. Task Manager initialization

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -17,3 +17,4 @@ fun main() {
 fun Application.module() {
     configureRouting()
 }
+
--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,38 +1,101 @@
 package com.example
 
+import model.*
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.response.*
+import io.ktor.server.request.*
 import io.ktor.server.routing.*
-import io.ktor.server.http.content.staticResources
+import io.ktor.server.http.content.*
 
 fun Application.configureRouting() {
-
-    // Встановлюємо StatusPages для обробки винятків
-    install(StatusPages) {
-        exception<IllegalStateException> { call, cause ->
-            call.respondText("App in illegal state as ${cause.message}")
-        }
-    }
-
     routing {
-        staticResources("/content", "mycontent")
+        staticResources("/task-ui", "task-ui")
 
-        get("/") {
-            call.respondText("Hello World!")
+        get("/tasks") {
+            val tasks = TaskRepository.allTasks()
+            call.respondText(
+                contentType = ContentType.parse("text/html"),
+                text = tasks.tasksAsTable()
+            )
         }
 
-        get("/test1") {
-            val text = "<h1>Hello From Ktor</h1>"
-            call.respondText(text, ContentType.Text.Html)
+        get("/byName/{taskName}") {
+            val name = call.parameters["taskName"]
+            if (name == null) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@get
+            }
+
+            val task = TaskRepository.taskByName(name)
+            if (task == null) {
+                call.respond(HttpStatusCode.NotFound)
+                return@get
+            }
+
+            call.respondText(
+                contentType = ContentType.parse("text/html"),
+                text = listOf(task).tasksAsTable()
+            )
         }
 
-        // Кінцева точка для тестування винятків
-        get("/error-test") {
-            throw IllegalStateException("Too Busy")
+        get("/byPriority/{priority?}") {
+            val priorityAsText = call.parameters["priority"]
+            if (priorityAsText == null) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@get
+            }
+
+            try {
+                val priority = Priority.valueOf(priorityAsText)
+                val tasks = TaskRepository.tasksByPriority(priority)
+
+                if (tasks.isEmpty()) {
+                    call.respond(HttpStatusCode.NotFound)
+                    return@get
+                }
+
+                call.respondText(
+                    contentType = ContentType.parse("text/html"),
+                    text = tasks.tasksAsTable()
+                )
+            } catch (ex: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
+        }
+
+        post("/tasks") {
+            val formContent = call.receiveParameters()
+
+            val params = Triple(
+                formContent["name"] ?: "",
+                formContent["description"] ?: "",
+                formContent["priority"] ?: ""
+            )
+
+            if (params.toList().any { it.isEmpty() }) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@post
+            }
+
+            try {
+                val priority = Priority.valueOf(params.third)
+                TaskRepository.addTask(
+                    Task(
+                        params.first,
+                        params.second,
+                        priority
+                    )
+                )
+                call.respond(HttpStatusCode.NoContent)
+            } catch (ex: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest)
+            } catch (ex: IllegalStateException) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
         }
     }
 }
+
 
 
